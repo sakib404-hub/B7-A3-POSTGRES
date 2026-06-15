c07b53259d1efbf57aa651aa5526dfbbe94caad9
sakib404-hub <akibhossainsakib1040@gmail.com>
2026-06-15 19:49:16 +0600

    query 4

--- Queries.sql
+++ Queries.sql
@@ -38,3 +38,16 @@ WHERE
 
 
 
+--Query 4: Retrieve match booking details along with the User's full name and the scheduled Match fixture teams.
+
+SELECT
+  booking_id,
+  full_name,
+  fixture,
+  total_cost
+FROM
+  bookings
+  INNER JOIN users USING (user_id)
+  INNER JOIN matches USING (match_id);
+
+
